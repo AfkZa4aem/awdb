e25500e1d04eab8c7c3a5e85b19833c83abd3333
Unknown <nastypinagrably@gmail.com>
2017-11-09 10:44:28 +0300

    update promises.js

--- Async Foundation/promise/chaining.js
+++ Async Foundation/promise/chaining.js
@@ -44,3 +44,27 @@ promise.then(function(data){
 }).then(function(data){
   console.log(data);
 });
+
+var counter = 0;
+function incCounter(){
+  counter++;
+  console.log("Counter:", counter);
+}
+
+function runLater(callback, timeInMs){
+  var p = new Promise(function(resolve, reject){
+    setTimeout(function(){
+      var res = callback();
+      resolve(res);
+    }, timeInMs);
+  });
+  return p;
+}
+
+runLater(incCounter, 1000).then(function(){
+  return runLater(incCounter, 2000);
+}).then(function(){
+  return runLater(incCounter, 3000);
+}).then(function(){
+  // final .then not necessary
+})
