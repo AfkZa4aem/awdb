5e84dff3a0991f6ea3363adf4df11e45075d78c4
Unknown <nastypinagrably@gmail.com>
2017-11-10 11:15:56 +0300

    Fetch error handling

--- AJAX/Fetch/firstExample/script.js
+++ AJAX/Fetch/firstExample/script.js
@@ -1,11 +1,28 @@
+// var url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+//
+// // make request
+// fetch(url)
+//   .then(function(res){
+//     // parse response
+//     return res.json();
+//   }).then(function(data){
+//     // get bitcoin price
+//     console.log(data.bpi.USD.rate);
+//   });
+
 var url = "https://api.coindesk.com/v1/bpi/currentprice.json";
 
-// make request
-fetch(url)
-  .then(function(res){
-    // parse response
-    return res.json();
-  }).then(function(data){
-    // get bitcoin price
-    console.log(data.bpi.USD.rate);
-  });
+fetch(url, {
+  method: "POST",
+  body: JSON.stringify({
+    name: 'blue',
+    login: 'bluecat'
+  })
+})
+.then(function(response){
+  console.log(response);
+  return response.json()
+})
+.then(function(data){
+  console.log(data.bpi.EUR.rate)
+});
