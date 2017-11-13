0058fc00c06be931b7d7fd3eeef0bc6115afe9dc
Unknown <nastypinagrably@gmail.com>
2017-11-13 09:41:09 +0300

    reafactoring

--- AJAX with jQuery/Axios/RonSwanson/script.js
+++ AJAX with jQuery/Axios/RonSwanson/script.js
@@ -2,7 +2,6 @@ var url = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
 var quote = document.querySelector("#quote");
 var xhrBtn = document.querySelector("#xhr");
 var fetchBtn = document.querySelector("#fetch");
-var jqueryBtn = document.querySelector("#jquery");
 var axiosBtn = document.querySelector("#axios");
 
 // XHR way
@@ -21,34 +20,21 @@ xhrBtn.addEventListener("click", function(){
 
 fetchBtn.addEventListener("click", function(){
   fetch(url)
-  .then(function(request){
-    if(!request.ok){
-      throw Error(request.status);
-    }
-    return request
-  })
   .then(function(res){
     return res.json().then(function(data){
-      return data
+      quote.innerText = data;
     });
   })
-  .then(function(data){
-    quote.innerText = data;
-  })
-})
+  .catch(function(){
+    alert("Error");
+  });
+});
 
 // jQuery way
-$(jqueryBtn).click(function(){
-  $.ajax({
-    method: "GET",
-    url: url,
-    dataType: "json"
-  })
+$("#jquery").click(function(){
+  $.getJSON(url)
   .done(function(data){
     $(quote).text(data);
-  })
-  .fail(function(){
-    console.log("Error!");
   });
 });
 
