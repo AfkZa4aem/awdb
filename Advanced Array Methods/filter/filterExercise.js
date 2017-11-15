ac230ab29a6806cf65ff5bad087ca009093a3c49
Unknown <nastypinagrably@gmail.com>
2017-11-15 21:57:25 +0300

    fix filterByValue function

--- Advanced Array Methods/filter/filterExercise.js
+++ Advanced Array Methods/filter/filterExercise.js
@@ -2,13 +2,14 @@
 Write a function called filterByValue which accepts an array of objects and a key and returns a new array with all the objects that contain that key.
 
 Examples:
-    filterByValue([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner') 
+    filterByValue([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner')
     // [{first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Colt', last:"Steele", isCatOwner: true}]
 */
 
 function filterByValue(arr, key){
 	return arr.filter(function(value){
-		return value[key] === true;
+		// return value[key] === true;
+		return val[key] !== undefined;
 	});
 }
 
@@ -31,7 +32,7 @@ function find(arr, searchValue){
 Write a function called findInObj which accepts an array of objects, a key, and some value to search for and returns the first found value in the array.
 
 Examples:
-    findInObj([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner',true) 
+    findInObj([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner',true)
     // {first: 'Tim', last:"Garcia", isCatOwner: true}
 */
 
