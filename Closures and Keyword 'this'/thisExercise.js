0697f83aea7e612955e6ac3718704441972b8165
Unknown <nastypinagrably@gmail.com>
2017-11-21 20:18:41 +0300

    add flip exercise

--- Closures and Keyword 'this'/thisExercise.js
+++ Closures and Keyword 'this'/thisExercise.js
@@ -128,8 +128,13 @@ Examples:
 
 */
 
-function bind(fn, thisArg){
-
+function flip(fn, thisArg){
+    var outerArgs = [].slice.call(arguments,2)
+    return function(){
+        var innerArgs = [].slice.call(arguments)
+        var allArgs = outerArgs.concat(innerArgs).slice(0, fn.length)
+        return fn.apply(thisArg, allArgs.reverse())
+    }
 }
 
 /*
