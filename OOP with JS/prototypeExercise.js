8253c884e744bb400108532cf665569750c2e179
Afkza4aem <nastypinagrably@gmail.com>
2017-11-23 19:47:36 +0300

    add exercises 1-5

---
+++ OOP with JS/prototypeExercise.js
@@ -0,0 +1,28 @@
+function Person(firstName, lastName, favoriteColor, favoriteNumber){
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.favoriteColor = favoriteColor;
+	this.favoriteNumber = favoriteNumber;
+	this.family = [];
+}
+
+Person.prototype.fullName = function(){
+	return this.firstName + " " + this.lastName;
+}
+
+Person.prototype.addToFamily = function(person){
+	if(person instanceof Person){
+		if(this.family.indexOf(person)){
+			this.family.push(person);
+		}
+	}
+	return this.family.length;
+}
+
+Array.prototype.map = function(callback){
+	var newArr = [];
+	for(var i = 0; i < this.length; i++){
+		newArr.push(callback(this[i], i, this));
+	}
+	return newArr;
+}
