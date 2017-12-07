0f878e81f522d7950e743077b63c583dd0c305a7
Unknown <nastypinagrably@gmail.com>
2017-12-07 18:17:11 +0300

    finish arrow function exercise

--- es2015/arrow function/exrcise.js
+++ es2015/arrow function/exrcise.js
@@ -56,10 +56,7 @@ Example:
     createStudentObj('Elie', 'Schoppik') // {firstName: 'Elie', lastName: 'Schoppik'}
 */
 
-function createStudentObj(firstName, lastName){
-  var newObj = {};
-  return newObj.firstName = lastName;
-}
+var createStudentObj = (firstName, lastName) => ({firstName:firstName, lastName:lastName});
 
 /* 5 - Given the following code:
 
@@ -76,3 +73,12 @@ Refactor this code to use arrow functions to make sure that in 1000 milliseconds
     }
 
 */
+
+var instructor = {
+  firstName: "Colt",
+  sayHi: function(){
+    setTimeout(() => {
+      console.log('Hello ' + this.firstName)
+    }, 1000)
+  }
+}
