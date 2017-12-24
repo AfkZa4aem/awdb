9cc0ae656e7b5d0e30b6e836e2755959e6910055
Unknown <nastypinagrably@gmail.com>
2017-12-24 19:44:17 +0300

    create hello world app
    update index, import pet statement

---
+++ React/helloworld/src/Pet.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Pet extends Component { 
+  render() {				
+    return (
+      <div className="card">
+        <h2 className="name">Moxie</h2>
+        <img src=" https://images.unsplash.com/photo-1482066490729-6f26115b60dc?auto=format&fit=crop&w=1002&q=80 " alt="Moxie " />
+        <h5 style={{fontSize: '2em', margin: '2px'}}>Hobbies:</h5>			
+        {/* <HobbyList /> */}
+      </div>
+    );
+  }
+}
+
+export default Pet;
